3b9e665fdb03a19677afb76272d38b1a90e5d300
Tangsonghuai <34538205+Tangsonghuai@users.noreply.github.com>
2021-03-20 17:32:59 +0800

    1

--- flink-learning-data-sources/src/main/java/com/zhisheng/data/sources/Main.java
+++ flink-learning-data-sources/src/main/java/com/zhisheng/data/sources/Main.java
@@ -16,15 +16,15 @@ public class Main {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("zookeeper.connect", "localhost:2181");
-        props.put("group.id", "metric-group");
+        props.put("bootstrap.servers", "192.168.2.120:9092");
+        props.put("zookeeper.connect", "min01:2181,min02:2182,min03:2183");
+        props.put("group.id", "metric01-group");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");  //key 反序列化
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("auto.offset.reset", "latest"); //value 反序列化
 
         DataStreamSource<String> dataStreamSource = env.addSource(new FlinkKafkaConsumer011<>(
-                "metric",  //kafka topic
+                "test06",  //kafka topic
                 new SimpleStringSchema(),  // String 序列化
                 props)).setParallelism(1);
 
